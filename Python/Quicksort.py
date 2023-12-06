4aacb290b9121cca836c5071762bd07132fba7c2
shreyasrajiv327 <shreyasrajiv327@gmail.com>
2023-12-06 23:07:48 +0530

    Update Quicksort

--- Python/Quicksort.py
+++ Python/Quicksort.py
@@ -1,39 +1,50 @@
 def partition(L, l, h):
     swap = 0
     count = 0
+    print("List while entering the partition:",L)
+    pivot = L[h]  # Choose the last element as the pivot
+    i = l - 1
 
-    pivot = L[l]
-    i = l
-    j = h
-
-    while True:
-        while L[i] < pivot:
-            count += 1
+    for j in range(l, h):
+        count += 1
+        print("Value of i",i,"and",j)
+        if L[j] <= pivot:
             i += 1
-        while L[j] > pivot:
-            count += 1
-            j -= 1
-        if i >= j:
-            return j, swap, count
-        L[i], L[j] = L[j], L[i]
+            if i != j:  # Count only if elements at two different indices exchange
+                print("Elements getting swapped are:",L[i],"and",L[j])
+                print("Value of i",i,"and",j)
+                print("List before swapping",L)
+                L[i], L[j] = L[j], L[i]
+                print("List after swapping",L)
+                swap += 1
+            
+
+    L[i + 1], L[h] = L[h], L[i + 1]
+    if i + 1 != h:  # Count only if elements at two different indices exchange
         swap += 1
+    #count += 1
+    print("List while exiting the partition:",L)
+    return i + 1, swap, count
 
 def quicksort(L, l, h):
     if l < h:
         j, swap, count = partition(L, l, h)
-        left_swap, left_count = quicksort(L, l, j)
+        left_swap, left_count = quicksort(L, l, j - 1)
         right_swap, right_count = quicksort(L, j + 1, h)
         return swap + left_swap + right_swap, count + left_count + right_count
     return 0, 0
 
-def main():
-    L = [1 ,3, 9, 8 ,2, 7, 5]
+def hacker_rank_format():
+    n = int(input())
+    L = list(map(int, input().split()))
+
     h = len(L) - 1
     l = 0
     total_swap, total_count = quicksort(L, l, h)
-    print(L)
-    print("No of swappings:", total_swap)
-    print("No of Comparisons:", total_count)
 
-main()
+    print(total_count)
+    print(total_swap)
+    print(" ".join(map(str, L)))
 
+if __name__ == "__main__":
+    hacker_rank_format()
