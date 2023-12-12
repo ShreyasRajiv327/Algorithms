22992b0d142a952e51ec766d02f2c1200d46a1cf
shreyasrajiv327 <shreyasrajiv327@gmail.com>
2023-12-12 15:46:38 +0530

    Updated to hackerank versiom

--- Hackerank/DijkshraAlgo.py
+++ Hackerank/DijkshraAlgo.py
@@ -10,21 +10,21 @@ else:
         adj[v1 - 1][v2 - 1] = w
     dist=[0]*vertices
     visited=[0]*vertices
-    for i in range(0,vertices):
+    for i in range(vertices):
         dist[i]=adj[0][i]
-    visited[1-1]=1
-    dist[1-1]=0
+    visited[0]=1
     n=0
-    while(n<vertices):
-        min=9999
-        for j in range(1,vertices):
-            if dist[j]<min and visited[j]==0:
-                min=dist[i]
+    while(n<vertices-1):
+        min_edge=9999
+        for j in range(vertices):
+            if dist[j]<min_edge and not visited[j]:
+                min_edge=dist[j]
                 v=j
         visited[v]=1
-        for k in range(1,vertices):
+        for k in range(vertices):
             if(dist[k]>dist[v]+adj[v][k]):
                 dist[k]=dist[v]+adj[v][k]
         n += 1
-    print(dist)
+    for y in range(1,len(dist)):
+        print(f"1 {y+1} {dist[y]}")
                 
